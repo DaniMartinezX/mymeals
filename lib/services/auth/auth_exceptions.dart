11abd6112ea8dc8d8348f9a83bb7ec7495a5c282
danifrx <danidelukas@gmail.com>
2023-05-09 13:40:04 +0200

    Auth Service

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,13 @@
+
+//ogin exceptions
+class UserNotFoundAuthException implements Exception{}
+class WrongPasswordAuthException implements Exception{}
+
+// Registrer exceptions
+class WeakPasswordAuthException implements Exception{}
+class EmailAlreadyInUseAuthException implements Exception{}
+class InvalidEmailAuthException implements Exception{}
+
+//Generic exceptions
+class GenericAuthException implements Exception{}
+class UserNotLoggedInAuthException implements Exception{}
